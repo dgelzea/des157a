e9b8103434eaaf5ab37c450878a89d5911d82a34
Gelzea Domingo <glzdomingo@ucdavis.edu>
2023-03-08 22:36:58 -0800

    update

--- game-on/script.js
+++ game-on/script.js
@@ -3,10 +3,12 @@
      'use script'
      console.log('js running');
 
-     const startGame = document.getElementById("startgame");
+     // const startGame = document.getElementById("startgame");
      const gameControl = document.getElementById("game-control");
      const game = document.getElementById("game");
      const actionArea = document.getElementById("actions");
+
+     const form = document.querySelector ("form");
 
      const score = document.getElementById("score");
      const p1Score = document.getElementById("p1-score");
@@ -15,13 +17,15 @@
 
      //sounds
      const startSound = new Audio('media/start.wav');
-     const rollSound = new Audio('media/dice-roll.mp3');
+     const rollSound = new Audio('media/dice-roll.wav');
      const alertSound = new Audio('media/alert.wav');
      const winSound = new Audio('media/win.wav');
 
      const gameData = {
           dice: ['images/1die.png', 'images/2die.png', 'images/3die.png', 'images/4die.png', 'images/5die.png', 'images/6die.png'], 
-          players: ['player one', 'player two'],
+          playerWin: ['images/player1-win.png', 'images/player2-win.png'],
+          // playerSwitch: ['images/player1-1.png', 'images/player2-1.png'],
+          // snakeEyes: ['images/player1-2.png', 'images/player2-2.png'],
           score: [0, 0],
           roll1: 0,
           roll2: 0,
@@ -30,128 +34,162 @@
           gameEnd: 29
      };
 
-     startGame.addEventListener("click", function(){
-          
-          document.getElementById("rules").style.display = "none";
-          document.getElementById("credits").style.display = "none";
+     form.addEventListener("submit", function(event){
+          event.preventDefault();
 
-          gameData.index = Math.round(Math.random());
-          gameControl.innerHTML = '<h3>The Game Has Started</h3>';
-          gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
+          const player1 = document.querySelector("#p1name").value;
+          const player2 = document.querySelector("#p2name").value;
 
-          document.getElementById('quit').addEventListener("click", function(){
-               location.reload();
-          });
+          const player1Img = document.getElementById("player-one");
+          const player2Img = document.getElementById("player-two");
 
-          setUpTurn();
-
-          //start sound
-          startSound.play();
-     });
-
-     function setUpTurn() {
-          game.innerHTML = `<h4>Roll the dice for ${gameData.players[gameData.index]}</h4>`;
-          actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
-
-          document.getElementById("roll").addEventListener("click", function(){
-
-               //start
-               throwDice();
-
-          });
-
-     };
-
-     function throwDice() {
-          actionArea.innerHtml = '';
-          gameData.roll1 = Math.floor(Math.random() * 6) + 1; 
-          gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-
-          game.innerHTML = `<h4>Roll the dice for ${gameData.players[gameData.index]}</h4>`;
-          game.innerHTML += `<img src = "${gameData.dice[gameData.roll1-1]}" alt="dice" width="120" height="120">`;
-          game.innerHTML += `<img src = "${gameData.dice[gameData.roll2-1]}" alt="dice" width="120" height="120">`;
-
-          gameData.rollSum =  gameData.roll1 + gameData.roll2;
-
-
-          //if two 1's are rolled . . .
-          if( gameData.rollSum === 2 ) {
-
-               game.innerHTML += '<h5>Oh snap! Snake eyes!</h5>';
-               gameData.score[gameData.index] = 0;
-               gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-
-               setTimeout(setUpTurn, 2000);
-
-               //alert sound
-               alertSound.play();
-
+          if(player1 == ''){
+               document.querySelector('#p1name').focus();
+               document.getElementById("error").innerHTML = `<span style=" color:#7895d0; font-weight:bold; text-transform: uppercase; ">please provide a name</span>`;
           }
-
-          //if either die is a 1 . . .
-          else if( gameData.roll1 === 1 || gameData.roll2 === 1 ){
-
-               gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-               game.innerHTML += `<h5>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</h5>`;
-
-               setTimeout(setUpTurn, 2000);
-               
-               //alert sound
-               alertSound.play();
-
-          }
-
-          //if neither die is a 1 . . .
-          else { 
-
-               gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-               actionArea.innerHTML = '<button id="rollagain">Roll Again</button> <button id="pass">Pass</button>';
-
-               document.getElementById("rollagain").addEventListener("click", function(){
-                    throwDice();
-               });
-
-               document.getElementById("pass").addEventListener("click", function(){
-                    gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                    setUpTurn();
-               });
-
-               //roll sound
-               rollSound.play();
-
-          }
-
-          showCurrentScore();
-          checkWinningCondition();
-
-          //roll sound
-          rollSound.play();
-
-     };
-
-     function checkWinningCondition() {
-          if(gameData.score[gameData.index] > gameData.gameEnd) {
-
-               win.innerHTML = `<h7>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h7>`
-
-               actionArea.innerHTML = '';
-               game.innerHTML = '';
-               document.getElementById('quit').innerHTML = "Start a New Game?";
-
-               //win sound
-               winSound.play();
+          else if(player2 == ''){
+               document.querySelector('#p2name').focus();
+               document.getElementById("error").innerHTML = `<span style=" color:#7895d0; font-weight: bold; text-transform: uppercase; ">please provide a name</span>`;
           }
           else {
-               showCurrentScore();
+  
+               const playerData = {
+                    players: [player1, player2],
+                    // playerImg: [player1Img, player2Img]
+               }
+
+               document.getElementById("rules").style.display = "none";
+               document.getElementById("credits").style.display = "none";
+               document.getElementById("game-start").style.display = "block";
+               gameControl.innerHTML = '';
+
+               gameData.index = Math.round(Math.random());
+
+               document.getElementById('quit').addEventListener("click", function(){
+                    location.reload();
+               });
+          
+               setUpTurn();
+               //start sound
+               startSound.play();
+
+
+               //set up who goes first
+               function setUpTurn(){
+
+                    game.innerHTML = `<h4>Roll the dice for <span style=" color:#7895d0; font-weight:bold; text-transform: uppercase; ">${playerData.players[gameData.index]}</span></h4>`;
+                    actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
+
+                    document.getElementById("roll").addEventListener("click", function(){
+                         //start
+                         throwDice();        
+                    });
+
+               };
+
+               //players turns
+               function throwDice() {
+
+                    actionArea.innerHtml = '';
+                    gameData.roll1 = Math.floor(Math.random() * 6) + 1; 
+                    gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+
+                    game.innerHTML = `<h4>Roll the dice for <span style=" color:#7895d0; font-weight:bold; text-transform: uppercase; ">${playerData.players[gameData.index]}</span></h4>`;
+                    actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
+                    game.innerHTML += `<img src = "${gameData.dice[gameData.roll1-1]}" alt="dice" width="120" height="120">`;
+                    game.innerHTML += `<img src = "${gameData.dice[gameData.roll2-1]}" alt="dice" width="120" height="120">`;
+
+                    gameData.rollSum =  gameData.roll1 + gameData.roll2;
+
+
+                    //if two 1's are rolled . . .
+                    if( gameData.rollSum === 2 ) {
+                         game.innerHTML += '<h5>Oh snap! Snake eyes!</h5>';
+
+                         gameData.score[gameData.index] = 0;
+                         gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+
+                         setTimeout(setUpTurn, 2000);
+
+                         //alert sound
+                         alertSound.play();
+                    }
+
+                    //if either die is a 1 . . .
+                    else if( gameData.roll1 === 1 || gameData.roll2 === 1 ){
+                         gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                         game.innerHTML += `<h5>Sorry, one of your rolls was a one, switching to ${playerData.players[gameData.index]}</h5>`;
+
+                         setTimeout(setUpTurn, 2000);
+                         //alert sound
+                         alertSound.play();
+                    }
+
+                    //if neither die is a 1 . . .
+                    else { 
+                         gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+                         actionArea.innerHTML = '<button id="rollagain">Roll Again</button> <button id="pass">Pass</button>';
+
+                         document.getElementById("rollagain").addEventListener("click", function(){
+                              throwDice();
+                         });
+
+                         document.getElementById("pass").addEventListener("click", function(){
+                              gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                              setUpTurn();
+                         });
+                         //roll sound
+                         rollSound.play();
+                    }
+
+                    showCurrentScore();
+                    checkWinningCondition();
+                    //roll sound
+                    rollSound.play();
+
+               };
+
+               //player winning
+               function checkWinningCondition() {
+
+
+                    if(gameData.score[gameData.index] > gameData.gameEnd) {
+
+                         win.innerHTML = `<h7><span style=" color:#72ad41;; font-weight:bold; text-transform: uppercase; ">${playerData.players[gameData.index]}</span> wins with <span style=" color:#72ad41;; font-weight:bold; text-transform: uppercase; ">${gameData.score[gameData.index]}</span> points!</h7>`
+
+                         win.innerHTML += `<img src = "${gameData.playerWin[gameData.index]}" alt="playerWin" width="130" height="130">`
+
+                         actionArea.innerHTML = '';
+                         game.innerHTML = '';
+                         score.innerHTML = '';
+                         document.getElementById('quit').innerHTML = "Start a New Game?";
+
+                         //win sound
+                         winSound.play();
+                    }
+                    else {
+                         showCurrentScore();
+                    }
+
+               }
+
+               //scoreboard
+               function showCurrentScore() {
+
+                    document.querySelector("#score").style.display="block";
+
+                    document.getElementById("player-one").innerHTML = `<img src = "images/player1.png" alt="player1" width="100" height="100" class="image">`;
+
+                    p1Score.innerHTML = `<h8><strong>${playerData.players[0]}: ${gameData.score[0]}</span></h8>`;
+
+                    document.getElementById("player-two").innerHTML = `<img src = "images/player2.png" alt="player1" width="100" height="100" class="image">`;
+
+                    p2Score.innerHTML = `<h8><strong>${playerData.players[1]}: ${gameData.score[1]}</strong></h8>`;
+
+               } 
+
           }
-     }
 
-     function showCurrentScore() {
-          document.querySelector("#score").style.display="block";
-
-          p1Score.innerHTML = `<h8><strong>${gameData.players[0]}: ${gameData.score[0]}</strong></h8>`;
-          p2Score.innerHTML = `<h8><strong>${gameData.players[1]}: ${gameData.score[1]}</strong></h8>`;
-     }
-
+     });
 
 })();
